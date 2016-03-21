87a01f76894fcb7c67fec3da785a8489df82a25c
Noitidart <noitidart@gmail.com>
2016-03-20 18:07:03 -0700

    1.1b.rev3
    * On uninstall/disable, it would not update in background. Fixed now so
    it does update in background.
    * It seems some stuff in e10s doesnt update in background and only
    updates on focus of tab, interesting, can dig into that in future

--- bootstrap.js
+++ bootstrap.js
@@ -25,51 +25,7 @@ var gObserves = {
 			var newZoom = Services.wm.getMostRecentWindow(null).ZoomManager.zoom;
 			console.log('newZoom:', newZoom);
 			
-			// get all currently open domains, and set site specific for each domain so they update in background, then remove all
-			var allDomains = new Set();
-			var domWins = Services.wm.getEnumerator('navigator:browser');
-			while (domWins.hasMoreElements()) {
-				var domWin = domWins.getNext();
-				var gbrowser = domWin.gBrowser;
-				var cntBrowsers = gbrowser.browsers;
-				for (var i=0; i<cntBrowsers; i++) {
-					// e10s safe way to check content of tab
-					console.log(i, gbrowser.browsers[i].currentURI.spec);
-					allDomains.add(CPS2.extractDomain(gbrowser.browsers[i].currentURI.spec));
-				}
-			}
-			
-			
-			var promiseAllArr_siteSpecificSet = [];
-			allDomains.forEach(function(domain) {
-				var deferred_siteSpecificSet = new Deferred();
-				promiseAllArr_siteSpecificSet.push(promiseAllArr_siteSpecificSet.promise);
-				
-				// set zoom for this domain
-				CPS2.set(domain, 'browser.content.full-zoom', newZoom, null, {
-					handleCompletion: function() {
-						console.log('ok complete, args:', arguments);
-						deferred_siteSpecificSet.resolve();
-					}
-				});
-			});
-			
-			var promiseAll_siteSpecificSet = Promise.all(promiseAllArr_siteSpecificSet);
-			promiseAll_siteSpecificSet.then(
-				function(aVal) {
-					console.log('Fullfilled - promiseAll_siteSpecificSet - ', aVal);
-					
-					// set global value
-					CPS2.setGlobal('browser.content.full-zoom', newZoom, null, {
-						handleCompletion: function() {
-							console.log('ok complete, args:', arguments);
-							// remove all site specific so each zoom goes to the global value of the one i just set
-							removeAllButGlobal(); // i put in the oncomplete, so it doesnt change it to what ever global is then bounce back to this new value
-						}
-					});
-				},
-				genericReject.bind(null, 'promiseAll_siteSpecificSet', 0)
-			).catch(genericCatch.bind(null, 'promiseAll_siteSpecificSet', 0));
+			applyZoomToAllDomains(newZoom);
 			
 		}
 	},
@@ -103,6 +59,65 @@ var gObserves = {
 	}
 }
 //end obs stuff
+function applyZoomToAllDomains(aNewZoom, boolDontSetGlobal, boolRemoveAll) {
+	// sets the zoom level of all currently open domains to aNewZoom
+		// including global zoom value
+	
+	// get all currently open domains, and set site specific for each domain so they update in background, then remove all
+	var allDomains = new Set();
+	var domWins = Services.wm.getEnumerator('navigator:browser');
+	while (domWins.hasMoreElements()) {
+		var domWin = domWins.getNext();
+		var gbrowser = domWin.gBrowser;
+		var cntBrowsers = gbrowser.browsers;
+		for (var i=0; i<cntBrowsers; i++) {
+			// e10s safe way to check uri of all browsers
+			console.log(i, gbrowser.browsers[i].currentURI.spec);
+			allDomains.add(CPS2.extractDomain(gbrowser.browsers[i].currentURI.spec));
+		}
+	}
+	
+	var promiseAllArr_siteSpecificSet = [];
+	allDomains.forEach(function(domain) {
+		var deferred_siteSpecificSet = new Deferred();
+		promiseAllArr_siteSpecificSet.push(promiseAllArr_siteSpecificSet.promise);
+		
+		// set zoom for this domain
+		CPS2.set(domain, 'browser.content.full-zoom', aNewZoom, null, {
+			handleCompletion: function() {
+				console.log('ok set for domain', domain, 'args:', arguments[0].domain);
+				deferred_siteSpecificSet.resolve();
+			}
+		});
+	});
+	
+	if (!boolDontSetGlobal) {
+		var deferred_globalSet = new Deferred();
+		promiseAllArr_siteSpecificSet.push(deferred_globalSet.promise);
+		CPS2.setGlobal('browser.content.full-zoom', aNewZoom, null, {
+			handleCompletion: function() {
+				console.log('ok complete, args:', arguments);
+				// remove all site specific so each zoom goes to the global value of the one i just set
+				deferred_globalSet.resolve(); // i put in the oncomplete, so it doesnt change it to what ever global is then bounce back to this new value
+			}
+		});
+	}
+	
+	var promiseAll_siteSpecificSet = Promise.all(promiseAllArr_siteSpecificSet);
+	promiseAll_siteSpecificSet.then(
+		function(aVal) {
+			console.log('Fullfilled - promiseAll_siteSpecificSet - ', aVal);
+			
+			if (boolRemoveAll) {
+				CPS2.removeByName('browser.content.full-zoom', null);
+			} else {
+				// remove all site specific so each zoom goes to the global value of the one i just set
+				removeAllButGlobal(); // i put in the oncomplete, so it doesnt change it to what ever global is then bounce back to this new value
+			}
+		},
+		genericReject.bind(null, 'promiseAll_siteSpecificSet', 0)
+	).catch(genericCatch.bind(null, 'promiseAll_siteSpecificSet', 0));
+}
 
 function removeAllButGlobal() {
 	
@@ -133,14 +148,14 @@ function uninstall(aData, aReason) {
 	console.log('uninstall aReason:', aReason);
 	if (aReason == ADDON_UNINSTALL) {
 		// reset the global zoom back to 1, otherwise when user resets zoom, then it will go to whatever was the last global setting
-		CPS2.removeByName('browser.content.full-zoom', null);
+		applyZoomToAllDomains(1, true, true);
 	}
 }
 
 function startup(aData, aReason) {
 	
 	Services.prefs.setBoolPref('browser.zoom.siteSpecific', true);
-	Services.prefs.setBoolPref('browser.zoom.updateBackgroundTabs', true); // i dont have to set thsi true, but if i dont then things arent instant, so lets just do it, as users might somehow have a false value
+	Services.prefs.setBoolPref('browser.zoom.updateBackgroundTabs', true); // i dont have to to this, if i dont, then things will update on tab focus. but its better for users, its what they expect. some novices might have this not set and will wonder why a window in the background didnt change zoom, even on focus, until they chagne tab then change back
 	
 	CPS2 = Cc['@mozilla.org/content-pref/service;1'].getService(Ci.nsIContentPrefService2);
 	
@@ -163,7 +178,7 @@ function shutdown(aData, aReason) {
 	console.log('shutdown aReason:', aReason);
 	if (aReason == ADDON_DISABLE) {
 		// reset the global zoom back to 1, otherwise when user resets zoom, then it will go to whatever was the last global setting
-		CPS2.removeByName('browser.content.full-zoom', null);
+		applyZoomToAllDomains(1, true, true);
 	}
 }
 
